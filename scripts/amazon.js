aea4c05340f039a6d2d817dba3b88644f4d80f6f
JustMarvee <emmanuelmarvelous579@gmail.com>
2023-11-02 21:35:03 +0100

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -76,6 +76,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
