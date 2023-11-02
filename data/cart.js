755a3c19b75e6f804c64942e6762958a9e7a1b8e
JustMarvee <emmanuelmarvelous579@gmail.com>
2023-11-02 21:24:33 +0100

    Make add to cart button interactive

---
+++ data/cart.js
@@ -0,0 +1,10 @@
+const cart = [];
+// data attribute
+// it allows us to attach any info to an element
+// it is another HTML attribute
+// They have to start with 'data-'
+// then we can give it any name
+// e.g. data-product-name="${product.name}"
+// .dataset collets a set of data from the data
+// attribute
+// button.dataset.productName
