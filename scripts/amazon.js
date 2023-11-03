da5ed7e85d1d19d4694ef75eb6a2c32734ef4ad1
JustMarvee <emmanuelmarvelous579@gmail.com>
2023-11-03 04:06:35 +0100

    Set timeout for adding to cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -41,7 +41,7 @@ products.forEach((product) => {
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+    <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
           </div>
@@ -89,5 +89,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    const addedMessage = document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
+
+    addedMessage.classList.add('added-to-cart-visible');
+
+    setTimeout(function () {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
   });
 });
